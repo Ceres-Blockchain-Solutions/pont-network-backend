899baf6ec7dcfcf50d355e06a935941d8633e660
RastkoCBS <rastko@ceresblockchain.solutions>
2024-09-04 14:39:00 +0200

    Remove auth and user service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,12 @@ import { AppService } from './app.service';
 import { ShipModule } from './ship/ship.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleModule } from '@nestjs/schedule';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/'),
     ScheduleModule.forRoot(),
     ShipModule,
-    AuthModule,
-    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
